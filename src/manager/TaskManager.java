23cbd30699e0d0630a524bfb3f35c32488bb4eb4
marzhan <abizhay.marzhan@gmai.com>
2024-06-17 12:00:24 +0500

    added Tests manager

---
+++ src/manager/TaskManager.java
@@ -0,0 +1,47 @@
+package manager;
+
+import task.Epic;
+import task.Subtask;
+import task.Task;
+
+import java.util.List;
+
+public interface TaskManager {
+    List<Task> getTasks();
+
+    void clearTasks();
+
+    Task getTask(int taskId);
+
+    Task createTask(Task task);
+
+    Task updateTask(Task task);
+
+    // Epic
+    List<Epic> getEpics();
+
+    void clearEpics();
+
+    Epic getEpic(int epicId);
+
+    Epic createEpic(Epic epic);
+
+    Epic updateEpic(Epic epic);
+
+    void deleteEpic(int epicId);
+
+    // Subtask
+    List<Subtask> getSubtasks();
+
+    void clearSubtasks();
+
+    Subtask getSubtask(int subtaskId);
+
+    Subtask createSubtask(Subtask subtask);
+
+    Subtask updateSubtask(Subtask subtask);
+
+    void deleteSubtask(int subtaskId);
+
+    List<Task> getHistory();
+}
